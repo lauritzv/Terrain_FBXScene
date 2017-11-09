8df1c7d40a0a88e60fddaf23206f7d42befdcf3e
Lauritz Valved <lauritzv@gmail.com>
2017-11-09 18:25:33 +0100

    vekk med stuen!  + et guffent tre

--- js/skybox.js
+++ js/skybox.js
@@ -1,7 +1,7 @@
 class Skybox{
 
-    constructor(pictureURL){
-
+    constructor(){
+/**
         if(pictureURL!=null)
             this.pictureURL = pictureURL;
         else
@@ -19,6 +19,24 @@ class Skybox{
 
         this.skyboxMesh = new THREE.Mesh(this.skyGeometry, this.skyMaterial);
         return this.skyboxMesh;
+*/
+
+        let imagePrefix = "./models/maps/skybox-";
+        let directions  = ["sides", "sides", "up", "dn", "sides", "sides"];
+        let imageSuffix = ".jpg";
+        let skyGeometry = new THREE.CubeGeometry( 100, 100, 100 );
+
+        let materialArray = [];
+        for (let i = 0; i < 6; i++)
+            materialArray.push( new THREE.MeshBasicMaterial({
+                map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+                side: THREE.BackSide,
+                fog: false
+            }));
+        let skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+        skyMaterial.fog = false;
+        this.skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+        return this.skyBox;
     }
 
 }
